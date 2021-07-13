d94500c880e1154b522591a8debff921166b8777
William Johnson <wlpj2011@gmail.com>
2021-07-12 21:15:05 -0400

    Part 2
    Map, tiles, and engine

---
+++ engine.py
@@ -0,0 +1,34 @@
+from typing import Set, Iterable, Any
+
+from tcod.context import Context
+from tcod.console import Console
+
+from entity import Entity
+from input_handlers import EventHandler
+from game_map import GameMap
+
+class Engine:
+    def __init__(self, entities: Set[Entity],event_handler: EventHandler, game_map: GameMap, player: Entity ):
+        self.entities = entities
+        self.event_handler = event_handler
+        self.game_map = game_map
+        self.player = player
+
+    def handle_events(self, events: Iterable[Any]) -> None:
+        for event in events:
+            action = self.event_handler.dispatch(event)
+
+            if action is None:
+                continue
+
+            action.perform(self,self.player)
+
+    def render(self, console: Console, context: Context)->None:
+        self.game_map.render(console)
+
+        for entity in self.entities:
+            console.print(entity.x, entity.y, entity.char, entity.color)
+
+        context.present(console)
+
+        console.clear()
--- main.py
+++ main.py
@@ -2,12 +2,17 @@
 import tcod
 from actions import EscapeAction, MovementAction
 from entity import Entity
+from engine import Engine
+from game_map import GameMap
 from input_handlers import EventHandler
 
 
 def main() -> None:
     screen_width = 80
     screen_height = 50
+
+    map_width = 80
+    map_height = 45
 
     tileset = tcod.tileset.load_tilesheet("dejavu10x10_gs_tc.png",
                                           32, 8, tcod.tileset.CHARMAP_TCOD)
@@ -18,6 +23,10 @@ def main() -> None:
     npc = Entity(screen_width//2 - 5, screen_height//2 , "@", (255,255,0))
     entities = {npc, player}
 
+    game_map = GameMap(map_width,map_height)
+
+    engine = Engine(entities = entities, event_handler = event_handler, game_map = game_map, player = player)
+
     with tcod.context.new_terminal(screen_width,
                                    screen_height,
                                    tileset=tileset,
@@ -25,23 +34,9 @@ def main() -> None:
                                    vsync = True) as context:
         root_console = tcod.Console(screen_width, screen_height, order = "F")
         while True:
-            root_console.print(x=player.x, y=player.y,string=player.char, fg = player.color)
-
-            context.present(root_console)
-
-            root_console.clear()
-
-            for event in tcod.event.wait():
-                action = event_handler.dispatch(event)
-
-                if action is None:
-                    continue
-
-                if isinstance(action, MovementAction):
-                    player.move(dx = action.dx, dy = action.dy)
-
-                elif isinstance(action,EscapeAction):
-                    raise SystemExit()
+            engine.render(console=root_console, context = context)
+            events = tcod.event.wait()
+            engine.handle_events(events)
 
 if __name__=="__main__":
     main()
