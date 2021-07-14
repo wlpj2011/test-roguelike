af83679466e6d8c70aa27db9b8efb837abf047ba
William Johnson <wlpj2011@gmail.com>
2021-07-14 19:21:48 -0400

    Update procgen.py
    fixed generation rate for health potions and fireball scrolls

--- procgen.py
+++ procgen.py
@@ -55,7 +55,7 @@ def place_entities(
         y = random.randint(room.y1 + 1,room.y2 - 1)
         if not any(entity.x == x and entity.y ==y for entity in dungeon.entities):
             item_chance = random.random()
-            if item_chance < 0.1:
+            if item_chance < 0.7:
                 entity_factories.health_potion.spawn(dungeon,x,y)
             elif item_chance < 0.8:
                 entity_factories.fireball_scroll.spawn(dungeon,x,y)
