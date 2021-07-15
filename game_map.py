285809b8b9909757307ea2170bda681ed1e2add7
William Johnson <wlpj2011@gmail.com>
2021-07-14 23:32:58 -0400

    Part 12
    Variable difficulty as dungeon level increase.

--- game_map.py
+++ game_map.py
@@ -22,8 +22,6 @@ class GameWorld:
         max_rooms: int,
         room_min_size: int,
         room_max_size: int,
-        max_monsters_per_room: int,
-        max_items_per_room: int,
         current_floor: int = 0,
     ):
         self.engine = engine
@@ -32,8 +30,6 @@ class GameWorld:
         self.max_rooms = max_rooms
         self.room_min_size = room_min_size
         self.room_max_size = room_max_size
-        self.max_monsters_per_room = max_monsters_per_room
-        self.max_items_per_room = max_items_per_room
         self.current_floor = current_floor
 
     def generate_floor(self)->None:
@@ -47,8 +43,6 @@ class GameWorld:
             room_max_size = self.room_max_size,
             map_width = self.map_width,
             map_height = self.map_height,
-            max_monsters_per_room = self.max_monsters_per_room,
-            max_items_per_room = self.max_items_per_room,
             engine = self.engine
         )
 
--- procgen.py
+++ procgen.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from typing import Iterator,List,Tuple,TYPE_CHECKING
+from typing import Dict, Iterator,List,Tuple,TYPE_CHECKING
 import random
 import tcod
 
@@ -9,6 +9,66 @@ import entity_factories
 
 if TYPE_CHECKING:
     from engine import Engine
+    from entity import Entity
+
+max_items_by_floor = [
+    (1,1),
+    (4,2),
+]
+
+max_monsters_by_floor = [
+    (1,2),
+    (4,3),
+    (6,5),
+]
+
+item_chances: Dict[int, List[Tuple[Entity,Int]]] = {
+    0: [(entity_factories.health_potion, 35)],
+    2: [(entity_factories.confusion_scroll,10)],
+    4: [(entity_factories.lightning_scroll,25)],
+    6: [(entity_factories.fireball_scroll,25)],
+}
+
+enemy_chances: Dict[int, List[Tuple[Entity,Int]]] = {
+    0: [(entity_factories.orc, 80)],
+    3: [(entity_factories.troll, 15)],
+    5: [(entity_factories.troll, 30)],
+    7: [(entity_factories.troll, 60)],
+}
+
+def get_max_value_for_floor(
+    weighted_chance_by_floor: List[Tuple[int,int]],
+    floor: int,
+)-> int:
+    current_value = 0
+    for floor_minimum, value in weighted_chance_by_floor:
+        if floor_minimum > floor:
+            break
+        else:
+            current_value = value
+    return current_value
+
+def get_entities_at_random(
+    weighted_chance_by_floor: Dict[int, List[Tuple[Entity,int]]],
+    number_of_entities: int,
+    floor: int,
+)->List[Entity]:
+    entity_weighted_chances = {}
+
+    for key, values in weighted_chance_by_floor.items():
+        if key > floor:
+            break
+        else:
+            for value in values:
+                entity = value[0]
+                weighted_chance = value[1]
+                entity_weighted_chances[entity] = weighted_chance
+    entities = list(entity_weighted_chances.keys())
+    entity_weighted_chances_values = list(entity_weighted_chances.values())
+    chosen_entities = random.choices(
+        entities, weights = entity_weighted_chances_values, k = number_of_entities,
+    )
+    return chosen_entities
 
 class RectangularRoom:
     """returns a rectangular room with a corner at x,y and with given height and width"""
@@ -36,33 +96,23 @@ class RectangularRoom:
 def place_entities(
     room: RectangularRoom,
     dungeon: GameMap,
-    maximum_monsters: int,
-    maximum_items: int,
+    floor_number: int,
     ) -> None:
-    number_of_monsters = random.randint(0,maximum_monsters)
-    number_of_items = random.randint(0,maximum_items)
-    for i in range(number_of_monsters):
-        x = random.randint(room.x1 + 1,room.x2 - 1)
-        y = random.randint(room.y1 + 1,room.y2 - 1)
-        if not any(entity.x == x and entity.y ==y for entity in dungeon.entities):
-            if random.random()<0.8:
-                entity_factories.orc.spawn(dungeon,x,y)
-            else:
-                entity_factories.troll.spawn(dungeon,x,y)
+    number_of_monsters = random.randint(0,get_max_value_for_floor(max_monsters_by_floor,floor_number))
+    number_of_items = random.randint(0,get_max_value_for_floor(max_items_by_floor,floor_number))
 
-    for i in range(number_of_items):
+    monsters: List[Entity] = get_entities_at_random(
+        enemy_chances, number_of_monsters, floor_number
+    )
+    items: List[Entity] = get_entities_at_random(
+        item_chances, number_of_items, floor_number
+    )
+
+    for entity in monsters + items:
         x = random.randint(room.x1 + 1,room.x2 - 1)
         y = random.randint(room.y1 + 1,room.y2 - 1)
         if not any(entity.x == x and entity.y ==y for entity in dungeon.entities):
-            item_chance = random.random()
-            if item_chance < 0.7:
-                entity_factories.health_potion.spawn(dungeon,x,y)
-            elif item_chance < 0.8:
-                entity_factories.fireball_scroll.spawn(dungeon,x,y)
-            elif item_chance < 0.9:
-                entity_factories.confusion_scroll.spawn(dungeon,x,y)
-            else:
-                entity_factories.lightning_scroll.spawn(dungeon,x,y)
+            entity.spawn(dungeon,x,y)
 
 def tunnel_between(start: Tuple[int,int],end: Tuple[int,int])->Iterator[Tuple[int,int]]:
     """Return an L-shaped tunnel between start and end"""
@@ -84,8 +134,6 @@ def generate_dungeon(max_rooms: int,
                     room_max_size: int,
                     map_width: int,
                     map_height: int,
-                    max_monsters_per_room: int,
-                    max_items_per_room: int,
                     engine: Engine)->GameMap:
     player = engine.player
     dungeon = GameMap(engine,map_width,map_height,entities = [player])
@@ -111,7 +159,7 @@ def generate_dungeon(max_rooms: int,
                 dungeon.tiles[x,y] = tile_types.floor
             center_of_last_room = new_room.center
 
-        place_entities(new_room,dungeon,max_monsters_per_room, max_items_per_room)
+        place_entities(new_room,dungeon,engine.game_world.current_floor)
         rooms.append(new_room)
 
         dungeon.tiles[center_of_last_room] = tile_types.down_stairs
